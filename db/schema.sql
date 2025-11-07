84f6a1c084b90eba148b588fc674d2692c4c2481
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-11-06 19:05:38 -0600

    feature: Database schemas

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
--- TODO
+DROP TABLE IF EXISTS orders_products CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  date DATE NOT NULL,
+  note TEXT,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  price DECIMAL NOT NULL
+);
+
+CREATE TABLE orders_products (
+  order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+  product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+  quantity INTEGER NOT NULL,
+    PRIMARY KEY (order_id, product_id) 
+);
