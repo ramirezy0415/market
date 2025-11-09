e750d8569706ca25ad0c5eca833e0bc27cd63051
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-11-09 16:19:21 -0600

    feature: POST /orders

--- api/orders.js
+++ api/orders.js
@@ -2,8 +2,9 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { getOrdersByUserById } from "#db/queries/orders";
+import { getOrdersByUserById, insertOrder } from "#db/queries/orders";
 import requireUser from "#middleware/requireUser";
+import requireBody from "#middleware/requireBody";
 
 import getUserFromToken from "#middleware/getUserFromToken";
 router.use(getUserFromToken);
@@ -14,6 +15,20 @@ router.get("/", async (req, res) => {
     const orders = await getOrdersByUserById(req.user.id);
     if (!orders) res.status(403).send("No orders under user ");
     return res.status(200).send(orders);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post("/", requireBody(["date"]), async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { date, note } = req.body;
+
+    const order = await insertOrder(date, note, id);
+    if (!order) return res.status(500).send("Unable to insert order.");
+
+    return res.status(201).send(order);
   } catch (error) {
     console.error(error);
   }
--- db/queries/orders.js
+++ db/queries/orders.js
@@ -1,16 +1,20 @@
 import db from "#db/client";
 
-export async function insertOrder(note, user_id) {
+export async function insertOrder(date, note, user_id) {
   try {
-    const current_date = new Date();
+    if (!date) {
+      date = new Date();
+    }
+
     const query = `
         INSERT INTO orders(date,note,user_id)
         VALUES($1,$2, $3)
         RETURNING *
     `;
+
     const {
       rows: [order],
-    } = await db.query(query, [current_date, note, user_id]);
+    } = await db.query(query, [date, note, user_id]);
 
     return order;
   } catch (error) {
